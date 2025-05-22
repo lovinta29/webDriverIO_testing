79f276f9d3dac23ff7282d1b9bcdc4b9d61cf67e
Lovinta Oktavia Hutagalung <103632183+lovinta29@users.noreply.github.com>
2025-05-22 19:18:53 +0700

    delete test1

--- test/specs/test.spec.js
+++
@@ -1,7 +0,0 @@
-describe ('Open WebdriverIO page', () => {
-    it ('should open browser and access page', async() => {
-        await browser.url('https://webdriver.io/');
-        await browser.pause(2000);
-        expect (browser).toHaveTitleContaining('WebdriverIO');
-    })
-})
